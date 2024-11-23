0212b65eb18a62bc2bdc9c1d946d594b771bf982
Abdullah Majeed <abdullahmajeed969@gmail.com>
2024-11-24 03:02:53 +0500

    Procfile created

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pollRoutes = require("./router/polls")
 const userRoutes = require("./router/user")
 const cors = require('cors')
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use((req, res, next) => {
@@ -14,8 +14,8 @@ app.use((req, res, next) => {
 app.use('/api/polls', pollRoutes);
 app.use('/api/user', userRoutes);
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(process.env.PORT_NO, () => {
-        console.log(`Backend service listening on ${process.env.PORT_NO}`)
+    app.listen(PORT, () => {
+        console.log(`Backend service listening on ${PORT}`)
     })
 }).catch((err) => {
     console.log("Failed to connect to database")
